3edf2ca7a28b79d196a4591bee1f15da2f735d5d
Sujal045 <sujalprajapati045@gmail.com>
2023-04-01 10:46:55 +0530

    url added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: "http://127.0.0.1:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
