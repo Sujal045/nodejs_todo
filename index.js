c74903cb0b55c51ba9cebea3d25f3230be087199
Sujal045 <sujalprajapati045@gmail.com>
2023-04-02 21:57:00 +0530

    frontend url added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: "https://react-app-bay.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
