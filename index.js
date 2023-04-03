a73c7a1f60aa7f94ffd844bd31dee7db51572805
Sujal045 <sujalprajapati045@gmail.com>
2023-04-03 09:35:05 +0530

    server

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://react-app-bay.vercel.app",
+    origin: "https://react-app-bay.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
